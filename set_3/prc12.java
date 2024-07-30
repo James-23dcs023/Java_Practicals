59282d2ace0aabceba583fc6dd5dfdaf38851b9c
James Dhandhukiya <23dcs023@charusat.edu.in>
2024-07-30 09:55:00 +0530

    Create prc12.java
    pound converter

---
+++ set_3/prc12.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class prc12
+{
+      public static void main(String a[])
+	{
+    int x=Integer.parseInt(a[0]);
+		int y=x*100;
+		System.out.println("The value is rupees is: "+y);
+	}
+}
